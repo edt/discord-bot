30c600fe5676aad54f0af48a685d2bcc2474d848
Edgar Thier <edgar.thier@theimagingsource.com>
2019-08-13 13:30:54 +0200

    Add !roll command
    Thanks to Malte for implementation

--- general.py
+++ general.py
@@ -7,6 +7,9 @@ import shutil
 
 import settings
 
+import re
+import random
+
 
 class General(commands.Cog):
     """
@@ -82,7 +85,72 @@ class General(commands.Cog):
         await ctx.send("Reboot scheduled.")
         settings.config.restart_scheduled = True
         # closes main loop. actual reboot has to be done there
+        log.info("REBOOT")
         await self.bot.close()
+
+    @commands.command()
+    async def upgrade(self, ctx):
+        """
+        Upgrade the bot
+
+        This command pull the latest version via git and reboots the bot.
+        """
+        await ctx.send("Not implemented")
+
+    def _roll(self, discord_string):
+
+        regex = r'(\d+)[dDwW](\d+)([+-]\d+)?$'
+        dice_string = re.search(regex, discord_string)
+
+        print(dice_string)
+
+        # Proceed if regex matched, send error message otherwise
+        if dice_string:
+            # initiate random number generator using OS randomizer
+            rng = random.SystemRandom()
+
+            # initiate variables and read number of dice and die sides from the first two regex groups
+            dice_num = int(dice_string.group(1))
+            dice_sides = int(dice_string.group(2))
+            dice_mod = 0
+            total = 0
+            result_list = []
+
+            # if a modifier exists
+            if dice_string.group(3):
+                dice_mod = int(dice_string.group(3))
+
+            # roll each die individually
+            for i in range(0, dice_num):
+                roll = rng.randrange(1, dice_sides + 1)
+                total += roll
+                result_list.append(roll)
+
+            # add modifier and output result
+            total += dice_mod
+
+            return True, total
+        else:
+            return False, 0
+
+    @commands.command()
+    async def roll(self, ctx, *args):
+        """
+        roll an arbitrary amount of dices.
+
+        Dices may have the format NdX+Y
+        N is the number of dices
+        X is the type of dice e.g. D20
+        Y is an arbitrary modifying int
+        """
+
+        for a in args:
+            ret, val = self.roll(a)
+
+            if ret:
+                await ctx.send("{} = {}".format(a, val))
+            else:
+                await ctx.send("Could not interpret '{}'".format(a))
 
 
 def setup(bot):
