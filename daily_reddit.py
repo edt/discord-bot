d7dc928d901e0e5744a2d179673a70d247ad8bc2
Edgar Thier <info@edgarthier.net>
2019-08-14 19:53:01 +0200

    Rework daily reddit
    split reddit and r command
    make daily posts working
    add/remove/list daily posts

--- daily_reddit.py
+++ daily_reddit.py
@@ -1,12 +1,49 @@
 import discord
 from discord.ext import commands
 
+import os
 import praw
 import secrets
-
+import sched
+import asyncio
+import time
+from datetime import datetime, timedelta
 import settings
 
-import inspect
+from tabulate import tabulate
+import json
+import sys
+
+import logging
+
+log = logging.getLogger(__file__)
+
+class RedditNewsletter():
+    """"""
+
+    def __init__(self, sub, chnl, time):
+
+        self.subreddit = sub
+        self.channel_id = chnl
+        self.post_time = time
+
+    def to_dict(self):
+
+        return {"subreddit": self.subreddit,
+                "channel_id": self.channel_id,
+                "post_time": self.post_time}
+
+    def __repr__(self):
+        return str(self.to_dict())
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    @staticmethod
+    def load(json_dump):
+        return RedditNewsletter(json_dump["subreddit"],
+                                json_dump["channel_id"],
+                                json_dump["post_time"])
 
 
 class DailyReddit(commands.Cog):
@@ -20,73 +57,173 @@ class DailyReddit(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-        self.reddit = praw.Reddit(client_id=settings.config.reddit_id,
-                                  client_secret=settings.config.reddit_secret,
-                                  user_agent=settings.config.reddit_agent)
+        self.praw = praw.Reddit(client_id=settings.config.reddit_id,
+                                client_secret=settings.config.reddit_secret,
+                                user_agent=settings.config.reddit_agent)
 
-        self.generate_functions()
+        self.newscycle = []
+        self.cache_file = "dailyreddit.json"
+        self.__load_newscycle()
 
-        self.subcommands = ["list", "remove", "add"]
+        for news in self.newscycle:
+            self.__start_newscycle(news)
 
-    @commands.group(aliases=['r'])
-    async def red(self, ctx, sub):
+    @commands.group()
+    async def reddit(self, ctx):
         """
         DailyReddit main command. Does nothing on its own.
         """
-        posts = self.reddit.subreddit(sub).hot(limit=30)
+        if ctx.invoked_subcommand is None:
+            await ctx.send(ctx.invoked_subcommand)
+
+    @commands.command()
+    async def r(self, ctx, sub):
+        """
+        Post random post of the current top 30 of the named subreddit
+        """
+
+        posts = self.praw.subreddit(sub).hot(limit=30)
         random_post_number = secrets.randbelow(30)
         for i, post in enumerate(posts):
             if i == random_post_number:
                 await ctx.message.channel.send(post.url)
-        # ctx.is_
-        await ctx.send("bla")
 
-    def __extract(self, subreddit):
+    def __start_newscycle(self, feed):
+        """"""
+        print("start_newscycle {}".format(feed.subreddit))
+
+        async def background_task():
+            await self.bot.wait_until_ready()
+            channel = self.bot.get_channel(int(feed.channel_id))
+            print("Checking if closed")
+            while not self.bot.is_closed():
+                fmt = "%H:%M"
+                now = datetime.strftime(datetime.now(), fmt)
+                
+                # calculate time in seconds
+                # if remaining time is negative
+                # we calculate it for the next day
+                remaining_time = (datetime.strptime(feed.post_time, fmt) 
+                                    - datetime.strptime(now, fmt)).total_seconds() 
+
+                if (remaining_time <= 0):
+                    remaining_time = (timedelta(hours=24) - (datetime.strptime(now, fmt) - datetime.strptime(post_time, fmt))).total_seconds() 
+                
+                print("going sleeping {} {}".format(feed.subreddit, remaining_time))
+                              
+                await asyncio.sleep(remaining_time)
+                
+                # security check
+                # we do not need to post when 
+                # task is interupted
+                if (self.bot.is_closed()
+                        or feed not in self.newscycle):
+                    return
+
+                print("sending")
+               
+                top_post = self.praw.subreddit(feed.subreddit).top('day')
+                for p in top_post:
+                   await channel.send("Daily {}: {}".format(feed.subreddit, p.url))
+                   break
+                # wait to ensure we only trigger once per task
+                await asyncio.sleep(60)
+                
+            print("end backgroundtask {}".format(feed.subreddit))
+
+        self.bot.loop.create_task(background_task())
+
+    def __load_newscycle(self):
         """
-        Extract currect top post.
+        Loads cache file
         """
-        top_post = self.reddit.subreddit("superbowl").top('day')
+        filename = os.path.join(settings.config.cache_dir, self.cache_file)
 
-        # for post in top_post:
-        #     await ctx.send(post.url)
+        with open(filename) as f:
 
-    @red.group()
+            data = json.load(f)
+
+        self.newscycle = [RedditNewsletter.load(dump)
+                          for dump in data]
+
+    def __save_newscycle(self):
+        """
+        Saves cache file
+        """
+        filename = os.path.join(settings.config.cache_dir, self.cache_file)
+
+        with open(filename, 'w+') as f:
+
+            json.dump([o.__dict__ for o in self.newscycle], f, indent=4, sort_keys=True)
+
+    @reddit.command()
     async def list(self, ctx):
         """
         List existing reddit posts
         """
+        table = []
+        table.append(["Index", "Subreddit", "Post Time"])
+        for news in self.newscycle:
 
-        await ctx.send("list")
+            table.append([self.newscycle.index(news),
+                          news.subreddit,
+                          news.post_time])
 
-    @red.group(pass_context=True)
-    async def add(self, ctx, time, subreddit):
+        await ctx.send("```{}```".format(tabulate(table, headers="firstrow")))
+
+    @reddit.command()
+    async def add(self, ctx, post_time, subreddit):
         """
         Add regular reddit post stuff
+
+        Time format: HH:MM
+        Subreddit is reddit.com/r/<string you need>
+
+        !reddit add 08:12 superbowl
+
+        The content will be posted in the channel the request was received.
         """
+        fmt = "%H:%M"
+        try:
+            datetime.strptime(post_time, fmt)
+        except ValueError as e:
+            await ctx.send("Could not parse time. {}".format(e))
+            return
+        
+        new_feed = RedditNewsletter(subreddit, ctx.channel.id, post_time)
+        self.newscycle.append(new_feed)
+        self.__save_newscycle()
+        self.__start_newscycle(new_feed)
+        await ctx.send("Added daily reddit post for {}".format(subreddit))
 
-        await ctx.send("not implented")
-
-    @red.group(pass_context=True)
+    @reddit.command()
     async def remove(self, ctx, entry):
         """
         Remove regular reddit post stuff
 
         Use the index of the !reddit list command
         """
+        try:
+            index = int(entry)
+        except: 
+            await ctx.send("Given index is not a number!")
+            return
 
-        await ctx.send("not implemented")
+        name = self.newscycle[index].subreddit
+        del self.newscycle[index]
+        await ctx.send("Removed entry for {}".format(name))
 
     def generate_cmd(self, cmd, helptext):
 
         @commands.command(name=cmd)
         async def red_(ctx):
-            posts = self.reddit.subreddit(cmd).hot(limit=30)
+            posts = self.praw.subreddit(cmd).hot(limit=30)
             random_post_number = secrets.randbelow(30)
             for i, post in enumerate(posts):
                 if i == random_post_number:
                     await ctx.message.channel.send(post.url)
 
-        self.red.add_command(red_)
+        self.reddit.add_command(red_)
 
     def generate_functions(self):
         """"""
@@ -95,7 +232,7 @@ class DailyReddit(commands.Cog):
         for cmd in cmds:
             self.generate_cmd(cmd, "")
 
-        inspect.getmembers(self.red)
+        inspect.getmembers(self.reddit)
 
 
 def setup(bot):
